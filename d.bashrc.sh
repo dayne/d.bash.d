d349a875973b869d97e4120e509f0cf5356c46b0
Dayne Broderson <broderson@gmail.com>
2024-03-22 11:35:43 -0800

    need local/bin to be a real thing first

--- d.bashrc.sh
+++ d.bashrc.sh
@@ -1,6 +1,14 @@
 
 # d.bash.d entry for .bashrc
 # cat d.bashrc.sh >> ~/.bashrc
+
+if [ -d $HOME/.local/bin ]; then
+  if ! echo $PATH | grep ".local/bin" > /dev/null; then
+    PATH=$PATH:$HOME/.local/bin
+    export PATH
+  fi
+fi
+
 if [ -d $HOME/.bash.d ]; then
   for I in $HOME/.bash.d/*.sh; do
     source $I
